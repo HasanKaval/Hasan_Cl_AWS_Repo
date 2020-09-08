5587f5fbc2beccd0671f4090e27d5e131280be59
hans90 <hasankaval77@gmail.com>
2020-09-09 01:22:04 +0200

    hellohello-world-app.py created

--- python/hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
+++ python/hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    return 'Hello it\'s me Hasan!'
-if __name__=='__main__':
-   app.run(host='0.0.0.0', port=80)
+
+@app.route("/")
+def hello() :
+    return <h1>Hello World</h1>
+
+if __name__ == "__main__" :
+    run.app(debug = True)
+    app.run(host = "0.0.0.0", port = 80)
