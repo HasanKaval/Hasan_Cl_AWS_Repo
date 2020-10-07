537c00ef1e9605e4bd41ce56cb2cc07c1da65404
hans90 <hasankaval77@gmail.com>
2020-10-07 23:57:12 +0200

    py code updated

--- python/coding-challenges/cc-006-generate-password/Generate password.py
+++ python/coding-challenges/cc-006-generate-password/Generate password.py
@@ -1,11 +1,11 @@
-name = input("Please enter your full name(without any space): ").lower()
-
 import random
 
 def get_random_letter():
+    name = input("Please enter your full name(without any space): ").lower()
     result = ''.join(random.choice(name) for i in range(3))
-    print(result, end='')
+    print(result + str(random.randrange(1000, 10000)))
+
 get_random_letter()
-print(random.randrange(1000, 10000))
+
     
     
