19509a79b75a170661241dfde096f6bb7c5d562c
hans90 <hasankaval77@gmail.com>
2020-12-27 21:31:00 +0100

    cleaned unneccesary folders and files

--- aws/class-notes/API & Lambda2/Hands-on-with-AWS-Lambda/Part-003_lambda/getSimpleRandomNumber.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-from random import randint
-
-print('Loading function')
-
-def lambda_handler(event, context):
-    myNumber = randint(0,100)
-    print("Random No. [ %s ]" % myNumber)
-    return myNumber
--- aws/class-notes/API & Lambda2/Hands-on-with-AWS-Lambda/Part-004_mapping_template/myHelloMsg.py
+++
@@ -1,5 +0,0 @@
-def lambda_handler(event, context):
-    # TODO implement
-    print("Event is %s" % event)
-    name = event.get("name") or "No msg submitted"
-    return "Hello from Lambda: %s " % (name)
